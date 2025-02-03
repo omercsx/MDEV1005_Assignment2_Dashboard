f91ee174cd7515ba8eefa093395ee7ce5eff710b
omercsx <omercagris@gmail.com>
2025-02-03 16:59:28 -0500

    Root redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
 import '@mantine/core/styles.css';
 
-import { MantineProvider } from '@mantine/core';
+import { Loader, MantineProvider } from '@mantine/core';
 import { Register } from './screens/Register';
-import { Route, Routes } from 'react-router';
+import { Navigate, Route, Routes } from 'react-router';
 import { Login } from './screens/Login';
 import { UserProvider } from './components/UserProvider';
 import { ProtectedRoute } from './screens/ProtectedRoute';
 import { Dashboard } from './screens/Dashboard';
+import { useUser } from './contexts/UserContext';
+
+function RootRedirect() {
+	const { user, loading } = useUser();
+
+	if (loading) {
+		return <Loader />;
+	}
+
+	return <Navigate to={user ? '/dashboard' : '/login'} replace />;
+}
 
 function App() {
 	return (
 		<MantineProvider>
 			<UserProvider>
 				<Routes>
+					<Route path='/' element={<RootRedirect />} />
 					<Route path='/register' element={<Register />} />
 					<Route path='/login' element={<Login />} />
 					<Route
